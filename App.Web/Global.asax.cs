2a1fcfac8e000b53a9f5be8f1184c580abf50788
ctgakram <akramul.net@gmail.com>
2020-04-16 11:28:04 +0600

    test

--- App.Web/Global.asax.cs
+++ App.Web/Global.asax.cs
@@ -27,7 +27,7 @@ namespace AppProj.Web
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
+            //
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
